16beb308cd7ccde534254db9e9c3c7b7d4193b4e
aircloud <onlythen@yeah.net>
2023-02-10 16:57:18 +0800

    feat: init

---
+++ packages/vite-code-replace/src/index.ts
@@ -0,0 +1,38 @@
+import fs from 'fs/promises'
+import type { PluginOption } from 'vite'
+
+export interface CodeReplaceOptions {
+  replaces: {
+    file: RegExp // 对哪些文件执行这个替换
+    source: RegExp | string
+    target: string | number | boolean
+  }[]
+}
+
+const loadAndReplaceFile = async (
+  id: string,
+  options: CodeReplaceOptions,
+): Promise<string | null> => {
+  let fileContent: string | null = null
+
+  for (const replaceConfig of options.replaces) {
+    if (!replaceConfig.file || replaceConfig.file?.test(id)) {
+      if (!fileContent) fileContent = (await fs.readFile(id, 'utf-8')) || ''
+      fileContent = (fileContent as string).replace(replaceConfig.source, `${replaceConfig.target}`)
+    }
+  }
+
+  /**
+   * hint: 根据 vite 当前的策略，这里如果直接返回 null 就相当于跳过这个插件，也是不会报错的
+   */
+  return fileContent
+}
+
+export function codeReplace(options: CodeReplaceOptions): PluginOption {
+  return {
+    name: 'path-load-file',
+    load(id: string) {
+      return loadAndReplaceFile(id, options)
+    },
+  }
+}
